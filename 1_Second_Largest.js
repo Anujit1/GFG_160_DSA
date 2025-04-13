3389fc4a9a10f632db1f488bbb5923cd0f54d043
Anujit Singh <anujit922@gmail.com>
2025-04-13 12:07:49 +0530

    Day1 code done!

---
+++ 1_Second_Largest.js
@@ -0,0 +1,22 @@
+let arr = [11, 2, 35, 55, 88, 31, 889]
+
+function getSecondLargest(arr) {
+        
+    let max = -1;
+    let max2 = -1;
+    
+    for(let i of arr){
+        if(max < i){
+            max2 = max
+            max = i;
+        }
+        
+        else if(i < max && i > max2){
+            max2 = i;
+        }
+    }
+    
+    return max2;
+}
+
+console.log(getSecondLargest(arr));
